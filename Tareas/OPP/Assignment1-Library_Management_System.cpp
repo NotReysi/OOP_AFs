94012624f267632e8422cd235edcd308d008304a
Nano <orlandoav.2006@gmail.com>
2025-09-06 20:29:12 -0600

    Logre que cpp se ejecutara correctamente y empece a hacer las clases para la actividad

--- Tareas/OPP/Assignment1-Library_Management_System.cpp
+++ Tareas/OPP/Assignment1-Library_Management_System.cpp
@@ -1,5 +1,40 @@
+/* 
+• Define classes for Book, Member, and Library.
+• Allow adding new books and members to the library.
+• Implement functionality for borrowing and returning books.
+• Ensure that a book cannot be borrowed if it is already checked out.
+• Display the list of available books and the borrowing status.
+• Use appropriate methods and attributes for each class.
+*/
 #include <iostream>
 #include <string>
+#include <vector>
+
+using namespace std;
+
+class Book {
+private:
+    string title;
+    string author;
+    int bookid;
+    bool borrowed;
+
+public:
+    
+};
+
+class Member {
+private:
+    string name;
+    int memberid;
+    vector<int> borrowedbooks;
+};
+
+class Library {
+private:
+    int totalbooks;
+    int totalmembers;
+};
 
 int main () {
 
