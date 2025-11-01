a0ef44a4bd22737c091a8962bfcabc5995cdddbf
Nano <orlandoav.2006@gmail.com>
2025-10-31 21:20:36 -0600

    Avance de la tercera tarea

--- Assignment3/Tarea3.cpp
+++ Assignment3/Tarea3.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
-//<algorithm>	Útil para funciones como std::max (para calcular la altura de subárboles) o potencialmente para la etapa de fusión de arreglos.
-//<vector>	Muy útil para almacenar temporalmente los elementos durante los recorridos y especialmente necesaria para la etapa de fusión de árboles (convertir el árbol a un array/vector ordenado).
-//<cmath>	Podría ser necesaria si decides usar alguna función matemática específica para el manejo de la precisión de double      (aunque a menudo se maneja con constantes y lógica booleana). 
-
-
-using namespace std;
-
-//max(a, b)	                Función auxiliar (o usando std::max) para obtener el mayor de dos valores (necesario para calcular la altura).
-//getHeight(node)	        Calcula la altura de un subárbol o nodo. ¡Debe ser eficiente! (generalmente se guarda en el nodo).
-//getBalanceFactor(node)	Calcula el factor de balanceo de un nodo (diferencia de alturas de subárboles izquierdo y derecho).
-//rightRotate(y)	        Implementa la rotación simple a la derecha para rebalancear.
-//leftRotate(x)	            Implementa la rotación simple a la izquierda para rebalancear.
-//insert(root, value)	    Función/Método privado que inserta recursivamente un valor y luego realiza las rotaciones necesarias para rebalancear.
-//insert(value)	            Método público que inicia la inserción llamando a la función privada.
 //search(root, value)	    Función/Método privado para buscar un valor en el árbol.
 //search(value)	            Método público que inicia la búsqueda.
 //inOrder(root)	            Recorre el árbol en orden: izquierda -> nodo -> derecha.
@@ -22,66 +11,169 @@ using namespace std;
 //deleteNode(root, value)	Implementa la eliminación de un nodo y el rebalanceo posterior. ¡El más complejo!
 //merge(tree1, tree2)	    El método que encapsula el proceso de fusión de dos árboles AVL.
 
+using namespace std;
 
-class Node{
-    private:
-        //  ATRIBUTES
-        double value;
-        int height;
-        Node* left;
-        Node* right;
+const double e = 1e-9;
 
-    public:
-        Node(double val){ //    CONSTRUCTOR
-            value = val;
-            height = 1;
-            left = nullptr;
-            right = nullptr;
-        }
+struct Node{
+    double value;
+    int height;
+    Node* left;
+    Node* right;
+    Node(double val){
+        value = val;
+        height = 1;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
 class Tree{
     private:
-        //  ATRIBUTES
         Node* root;
+
+        int getHeight(Node* node){
+            if(node == nullptr){
+                return 0;
+            }
+            else{
+                return node->height;
+            }
+        }
+
+        int getFE(Node* node){
+            if(node == nullptr){
+                return 0;
+            }
+            else{
+                return getHeight(node->left) - getHeight(node->right);
+            }
+        }
+
+        Node* rightRotate(Node* y){
+            Node* x = y->left;
+            Node* aux = x->right;
+            x->right = y;
+            y->left = aux;
+
+            x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+            y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+            return x;
+        }
+
+        Node* leftRotate(Node* x){
+            Node* y = x->right;
+            Node* aux = y->left;
+            y->left = x;
+            x->right = aux;
+
+            x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+            y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+            return y;
+        }
+
+        Node* insert(Node* node, double value){
+            if (node == nullptr){
+                return new Node(value);
+            }
+
+            if(abs(value - node->value) < e){
+                return node;
+            }
+            else if (value < node->value){
+                node->left = insert(node->left, value);
+            }
+            else {
+                node->right = insert(node->right, value);
+            }
+            
+            node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+            int balance = getFE(node);
+
+            if (balance > 1 && getFE(node->left) >= 0){
+                return rightRotate(node);
+            }
+            if (balance < -1 && getFE(node->right) <= 0){
+                return leftRotate(node);
+            }
+            if (balance > 1 && getFE(node->left) < 0){
+                node->left = leftRotate(node->left);
+                return rightRotate(node);
+            }
+            if (balance < -1 && getFE(node->right) > 0){
+                node->right = rightRotate(node->right);
+                return leftRotate(node);
+            }
+
+            return node;
+        }
+
+        void inorder(Node* node){
+            if (node == nullptr){
+                return;
+            }
+            else{
+                inorder(node->left);
+                cout << node->value << "";
+                inorder(node->right);
+            }
+        }
+
+        void destroy(Node* node){
+            if (node == nullptr){
+                return;
+            }
+            else{
+                destroy(node->left);
+                destroy(node->right);
+                delete node;
+            }
+        }
+
+        void search(Node* node, double value){
+            
+        }
+
+        void errase(Node* node, double value){
+            
+        }
 
 
 
     public:
-        Tree(){ //    CONSTRUCTOR
+        Tree(){
             root = nullptr;
         }
-
-        double insert(double value){
-
+        
+        ~Tree(){
+            destroy(root);
         }
 
-        double search(double value){
-            
+        void insert(double value){
+            root = insert(root, value);
         }
 
-        void order(){
-
+        void inorder(){
+            inorder(root);
+            cout << endl;
         }
 
-        int getHeight(){
-            
+        void search(double value){
+            search(value);
         }
 
-        void leftRotate(){
-
+        void errase(double value){
+            errase(root, value);
         }
-
-        void _insert(Node* node, double value){
-
-        }
-
 };
 
 
-
-
+//falta:
+//Borrar valores
+//Buscar valores
+//Fusionar 2 arboles
 
 int main(){
+    Tree tree;
 
 }
