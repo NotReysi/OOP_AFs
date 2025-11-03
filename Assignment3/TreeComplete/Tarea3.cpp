ccc1cbd8e74b365f887a588e23c18b309872331b
Nano <orlandoav.2006@gmail.com>
2025-11-02 22:41:31 -0600

    Organicé bien las carpetas y comenté los códigos

--- Assignment3/TreeComplete/Tarea3.cpp
+++ Assignment3/TreeComplete/Tarea3.cpp
@@ -6,13 +6,19 @@
 
 using namespace std;
 
+//Defined a global constant "e" (epsilon) to compare double values
+//This helps to get rid of the imprecision
 const double e = 1e-9;
 
+// ---------- Definition of the Node structure ----------
 struct Node{
-    double value;
-    int height;
-    Node* left;
-    Node* right;
+    //---------- Atributes ----------
+    double value;  // The value stored in the node
+    int height;    // The height of the subtree rooted at this node
+    Node* left;    // Pointer to the left child
+    Node* right;   // Pointer to the right child
+
+    // Constructor
     Node(double val){
         value = val;
         height = 1;
@@ -21,10 +27,12 @@ struct Node{
     }
 };
 
+// ---------- Definition of the Tree class ----------
 class Tree{
     private:
-        Node* root;
+        Node* root; // The main root of the tree
 
+        // -- Get height of a node --
         int getHeight(Node* node){
             if(node == nullptr){
                 return 0;
@@ -33,7 +41,8 @@ class Tree{
                 return node->height;
             }
         }
-
+        
+        // -- Get Balance Factor (FE) --
         int getFE(Node* node){
             if(node == nullptr){
                 return 0;
@@ -43,64 +52,76 @@ class Tree{
             }
         }
 
+        // -- Performs a right rotation --
         Node* rightRotate(Node* y){
             Node* x = y->left;
             Node* aux = x->right;
-            x->right = y;
-            y->left = aux;
 
+            // Perform rotation
+            x->right = y;
+            y->left = aux;  // 'aux' becomes the left child of 'y'
+
+            // Update heights
             x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
             y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
             return x;
         }
 
+        // -- Performs a left rotation --
         Node* leftRotate(Node* x){
             Node* y = x->right;
             Node* aux = y->left;
-            y->left = x;
-            x->right = aux;
 
+            // Perform rotation
+            y->left = x;
+            x->right = aux; // 'aux' becomes the right child of 'x'
+
+            // Update heights
             x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
             y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
             return y;
         }
 
+        // -- Recursive function to insert a node -- 
         Node* insert(Node* node, double value){
-            if (node == nullptr){
+            if (node == nullptr){   // Find leaf position and insert
                 return new Node(value);
             }
 
-            if(abs(value - node->value) < e){
+            if(abs(value - node->value) < e){   // Check for duplicates
                 return node;
             }
-            else if (value < node->value){
+            else if (value < node->value){  // Go left or right
                 node->left = insert(node->left, value);
             }
             else {
                 node->right = insert(node->right, value);
             }
             
+            // Update height of this ancestor node
             node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
-            int balance = getFE(node);
+            int balance = getFE(node);  // Get balance factor
 
-            if (balance > 1 && getFE(node->left) >= 0){
+            // -- Rebalance if needed (4 cases) -- 
+            if (balance > 1 && getFE(node->left) >= 0){     // Left-Left Case
                 return rightRotate(node);
             }
-            if (balance < -1 && getFE(node->right) <= 0){
+            if (balance < -1 && getFE(node->right) <= 0){   // Right-Right Case
                 return leftRotate(node);
             }
-            if (balance > 1 && getFE(node->left) < 0){
+            if (balance > 1 && getFE(node->left) < 0){      // Left-Right Case
                 node->left = leftRotate(node->left);
                 return rightRotate(node);
             }
-            if (balance < -1 && getFE(node->right) > 0){
+            if (balance < -1 && getFE(node->right) > 0){    // Right-Left Case
                 node->right = rightRotate(node->right);
                 return leftRotate(node);
             }
 
             return node;
         }
-
+    
+        // -- Inorder Traversal (L, Root, R) --
         void inorder(Node* node){
             if (node == nullptr){
                 return;
@@ -111,7 +132,8 @@ class Tree{
                 inorder(node->right);
             }
         }
-
+        
+        // -- Preorder Traversal (Root, L, R) --
         void preorder(Node*node){
             if (node == nullptr){
                 return;
@@ -123,6 +145,7 @@ class Tree{
             }
         }
 
+        // -- Postorder Traversal (L, R, Root) --
         void postorder(Node* node){
             if (node == nullptr){
                 return;
@@ -133,7 +156,8 @@ class Tree{
                 cout << node->value << "  ";
             }
         }
-
+        
+        // -- Destroy tree (free memory) --
         void destroy(Node* node){
             if (node == nullptr){
                 return;
@@ -141,27 +165,29 @@ class Tree{
             else{
                 destroy(node->left);
                 destroy(node->right);
-                delete node;
+                delete node;    // Delete after children
             }
         }
 
+        // -- Recursive function to search a node -- 
         Node* search(Node* node, double value){
-            if (node == nullptr){
+            if (node == nullptr){               // Not found
                 return nullptr;
             }
-            if(abs(value - node->value) < e){
+            if(abs(value - node->value) < e){   // Found
                 return node;
             }
-            else if (value < node->value){
+            else if (value < node->value){      // Go left
                 return search(node->left, value);
             }
-            else {
+            else {                              // Go right
                 return search(node->right, value);
             }
         }
 
+        // -- Recursive function to errase a node -- 
         Node* errase(Node* node, double value){
-            if (node == nullptr){
+            if (node == nullptr){   // Find the node
                 return nullptr;
             }
             
@@ -171,33 +197,40 @@ class Tree{
             else if (value > node->value){
                 node->right = errase(node->right, value);
             }
+            // Node found, start deletion logic
             else if(abs(value - node->value) < e){
+                // Case 1: No children (leaf node)
                 if (node->left == nullptr && node->right == nullptr){
                     delete node;
                     return nullptr;
                 }
+                // Case 2: Two children
                 else if(node->left != nullptr && node->right != nullptr){
+                    // Find smallest node in right subtree
                     Node* current = node->right;
                     while (current->left != nullptr) {
                         current = current->left;
                     }
-                    node->value = current->value;
-                    node->right = errase(node->right, current->value);
+                    node->value = current->value;   // Copy successor's value
+                    node->right = errase(node->right, current->value);  // Delete the successor from the right subtree
                 }
+                // Case 3: One child
                 else{
+                    // Get the single child
                     if (node->left != nullptr){
                         Node* temp = node->left;
                         delete node;
-                        return temp;
+                        return temp;    // Return child to parent
                     }
                     else{
                         Node* temp = node->right;
                         delete node;
-                        return temp;
+                        return temp;    // Return child to parent
                     }
                 }
             }
             
+            // Rebalance (Same as insert function)
             if (node == nullptr){
                 return nullptr;
             }
@@ -223,6 +256,7 @@ class Tree{
             return node;
         }
 
+        // -- Convert the Tree to a vector --
         void to_vector(Node* node, vector<double>& current_tree){
             if (node == nullptr){
                 return;
@@ -234,90 +268,103 @@ class Tree{
             }
         }
 
+        // -- Build a tree from a vector --
         Node* from_vector(const vector<double>& vec, int start, int end){
             if (start > end){
                 return nullptr;
             }
-            int mid = start + (end - start) / 2;
-            Node* node = new Node(vec[mid]);
+            int mid = start + (end - start) / 2;    // Find middle
+            Node* node = new Node(vec[mid]);        // Middle is root
 
-            node->left = from_vector(vec, start, mid - 1);
+            // Build subtrees
+            node->left = from_vector(vec, start, mid - 1);  
             node->right = from_vector(vec, mid + 1, end);
 
+            // Update height
             node->height = 1 + max(getHeight(node->left), getHeight(node->right));
 
             return node;
         }
-
+    
+    // --- Public Interface ---
     public:
+        // Constructor
         Tree(){
             root = nullptr;
         }
         
+        // Destructor
         ~Tree(){
             destroy(root);
         }
 
+        // -- Public insert --
         void insert(double value){
             root = insert(root, value);
         }
 
+        // -- Public inorder --
         void inorder(){
             inorder(root);
             cout << endl;
         }
 
+        // -- Public preorder --
         void preorder(){
             preorder(root);
             cout << endl;
         }
 
+        // -- Public postorder --
         void postorder(){
             postorder(root);
             cout << endl;
         }
 
+        // -- Public search --
         bool search(double value){
-            return search(root, value) != nullptr;
+            return search(root, value) != nullptr;  // Return true if found
         }
 
+        // -- Public erase --
         void errase(double value){
             root = errase(root, value);
         }
 
+        // -- Merge two trees --
         void merge(Tree& otherTree){
+            // Save this tree to vec1
             vector<double> vec1;
             to_vector(this->root, vec1);
 
+            // Save other tree to vec2
             vector<double> vec2;
             to_vector(otherTree.root, vec2);
 
+            // Merge sorted vectors
             vector<double> merged;
             std::merge(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), back_inserter(merged));
 
+            // Destroy and rebuild this tree
             destroy(this->root);
-
             this->root = from_vector(merged, 0, merged.size() - 1);
 
+            // Empty the other tree
             otherTree.destroy(otherTree.root);
             otherTree.root = nullptr;
         }
 };
 
+// ---------- Main function ----------
 int main(){
-    Tree tree;
+    Tree tree;  // Create tree instance
 
-    tree.insert(15);
-    tree.insert(0.5);
-    tree.insert(5);
-    tree.insert(1.32);
-    tree.insert(7.3);
-    tree.insert(0.0000003);
-
+    // Menu variables
     int selection;
     double val;
 
-    do{
+    do{ // Menu loop
+        // Print menu options
         cout << "\n                   Little Garden\n"<<
                 "----------------------WELCOME----------------------\n" <<
                 "\nPick a number to select any of this options:\n" <<
@@ -330,13 +377,13 @@ int main(){
         cout << "\nSelection: "; 
         cin >> selection;
 
-        switch (selection){
-            case 0:{
+        switch (selection){     // Handle selection
+            case 0:{            // Exit
                 cout << "\nHave a good day!!!\n";
                 cout << "\n=====================    CLOSING SYSTEM    =====================\n";
                 break;
             }
-            case 1:{
+            case 1:{            // Insert
                 cout << "\n----------------------NEW VALUE----------------------\n";
                 cout << "\nPlease enter the value: ";
                 cin >> val;
@@ -345,7 +392,7 @@ int main(){
                 cout << "\n-----------------------------------------------------\n";
                 break;
             }
-            case 2:{
+            case 2:{            // Search
                 cout << "\n------------------------SEARCH-----------------------\n";
                 cout << "Enter the value you want to search: ";
                 cin >> val;
@@ -359,11 +406,11 @@ int main(){
                 cout << "\n-----------------------------------------------------\n";
                 break;
             }
-            case 3:{
+            case 3:{            // Delete
                 cout << "\n--------------------DELETE VALUES--------------------\n";
                 cout << "Enter the value you want to errase: ";
                 cin >> val;
-                if (!tree.search(val)){
+                if (!tree.search(val)){     // Check if value exists
                     cout << val << " does not exist in the Tree D:\n";
                 }
                 else {
@@ -373,7 +420,7 @@ int main(){
                 cout << "\n-----------------------------------------------------\n";
                 break;
             }
-            case 4:{
+            case 4:{            // Display
                 cout << "\n---------------------INSIDE TREE---------------------\n";
                 cout << "Tree inorder: " << endl;
                 cout << "   ";
@@ -387,20 +434,22 @@ int main(){
                 cout << "\n-----------------------------------------------------\n";
                 break;
             }
-            case 5:{
+            case 5:{            // Merge
                 cout << "\n-----------------------MERGING-----------------------\n";
-                Tree otherTree;
+                Tree otherTree; // Create a temporary tree
                 int amount;
                 int merge_selection;
                 cout << "\nPlease enter the amount of values: ";
                 cin >> amount;
 
+                // Fill the temp tree
                 for (int i = 0; i < amount; i++){
                     cout << "Value " << i + 1 << " : ";
                     cin >> val;
                     otherTree.insert(val);
                 }
 
+                // Confirm the temp tree
                 cout << "\nThanks, this is the Tree inorder about to merge: " << endl;
                 cout << "   ";
                 otherTree.inorder();
@@ -411,6 +460,7 @@ int main(){
                 cout << "Selection: ";
                 cin >> merge_selection;
 
+                // Merge Trees
                 if (merge_selection == 1){
                     cout << "\n...Merging Trees...\n";
                     tree.merge(otherTree);
@@ -436,7 +486,7 @@ int main(){
                 cout << "\n############     Please select a number between 0 and 5  >:(     ############\n";
             }
         }
-    }while(selection != 0);
+    }while(selection != 0); // Repeat until user selects 0
 
     return 0;
 }
--- Assignment4/HeapComplete/Tarea4.cpp
+++ Assignment4/HeapComplete/Tarea4.cpp
@@ -4,69 +4,91 @@
 
 using namespace std;
 
+// ---------- Definition of the Task structure ----------
 struct Task{
-    int priority;
-    string description;
+    //---------- Atributes ----------
+    int priority;       // Lower number means higher priority
+    string description; // Description of the task
     
+    // Constructor
     Task(int priority, string description){
         this->priority = priority;
         this->description = description;
     }
+
+    // Display Task information
     void display(){
         cout << priority << "  -  " << description << endl;
     }
 
+    // Return Priority
     int getPriority(){
         return priority;
     }
 };
 
+// ---------- Definition of the Schedule class ----------
 class Schedule{
     private:
-        vector<Task> heap;
+        vector<Task> heap;  // Definition of the vector that stores the elements
 
+        // -- Move a node UP to its correct spot after insert --
         void heapify_Up(int i){
+            // While not at the root AND parent is bigger than me...
             while (i > 0 && heap[(i-1)/2].getPriority() > heap[i].getPriority()){
+                // ...swap me with my parent
                 swap(heap[(i-1)/2], heap[i]);
-                i = (i-1)/2;
+                i = (i-1)/2;    // Move up to the parent's index
             }
         }
 
+        // -- Move a node DOWN to its correct spot after extract --
         void heapify_Down(int i){
-            int n = heap.size();
+            int n = heap.size();    // Total number of elements
 
+            // While the node has at least one child (left child)
             while (2*i + 1 < n){
-                int j = 2*i + 1;
+                int j = 2*i + 1;    // Get left child index
+                // If a right child exists AND it's smaller than the left...
                 if (j + 1 < n && heap[j+1].getPriority() < heap[j].getPriority()){
-                    j++;
+                    j++;    // ...then 'j' becomes the index of the right child
                 }
+                // 'j' is now the index of the SMALLEST child
+
+                // If I am already smaller than my smallest child...
                 if (heap[i].getPriority() <= heap[j].getPriority()){
-                    break;
+                    break;  // ...my position is correct. Stop.
                 }
 
+                // Swap me with my smallest child
                 swap(heap[i], heap[j]);
-                i = j;
+                i = j;  // Move down to the child's index and repeat
             }
         }
 
+    // --- Public Interface ---
     public:
+        // ---- Add a new task ----
         void insert(int priority, string description){
-            heap.push_back(Task(priority, description));
-            heapify_Up(heap.size()-1);
+            heap.push_back(Task(priority, description));    // Add task to the very end of the vector
+            heapify_Up(heap.size()-1);                      // Bubble it up to its correct position
         }
 
+        // -- Check if heap is empty --
         bool isEmpty(){
             return heap.empty();
         }
 
+        // ---- Remove a task ----
         Task extract(){
-            Task minPriority = heap[0];
-            heap[0] = heap.back();
-            heap.pop_back();
-            heapify_Down(0);
-            return minPriority;
+            Task minPriority = heap[0]; // Save the root (min value) to return later
+            heap[0] = heap.back();      // Move the LAST element to the root
+            heap.pop_back();            // Remove the last element (which is now duplicated at the root)
+            heapify_Down(0);            // Bubble the new root down to its correct spot
+            return minPriority;         // Return the task we saved
         }
 
+        // ---- Display all the agenda ----
         void display(){
             for(int i = 0; i < heap.size(); i++){
                 heap[i].display();
@@ -74,10 +96,12 @@ class Schedule{
         }
 };
 
+// ---------- Main function ----------
 int main(){
-    Schedule task;
+    Schedule task;  // Create heap instance
     int selection;
-    do{
+    do{ // Menu loop
+        // Print menu options
         cout << "\n\n----------------------SCHEDULE----------------------\n";
         cout << "\nWhat would you like to do?\n" <<
                 "1) Add a Task.\n" << 
@@ -87,17 +111,17 @@ int main(){
         cout << "\nSelection: "; 
         cin >> selection;
 
-        switch(selection){
-            case 0:{
+        switch(selection){  // Handle selection
+            case 0:{        // Exit
                 cout << "\nHave a good day!!!\n";
                 break;
             }
-            case 1:{
+            case 1:{        // Add Task
                 int priority;
                 string description;
                 cout << "\n----------------------NEW TASK----------------------\n";
                 cout << "\nEnter the new Task: ";
-                getline(cin >> ws, description);
+                getline(cin >> ws, description);    // Read full line (skip whitespace)
                 cout << "Select the task priority:\n" <<
                         "1) Due Today\n" <<
                         "2) Due Tomorrow\n" <<
@@ -105,30 +129,30 @@ int main(){
                         "8 -> ...) Other priority\n";
                 cout << "\nPriority: ";
                 cin >> priority;
-                cin.ignore();
-                task.insert(priority, description);
+                cin.ignore();   // Clear newline from input
+                task.insert(priority, description); // Call heap insert
                 cout << "\n----------------------------------------------------\n";
                 break;
-            }
-            case 2:{
+            }   
+            case 2:{        // Remove Task
                 cout << "\n---------------------REMOVE TASK--------------------\n";
-                if (task.isEmpty()){
+                if (task.isEmpty()){    // Check if empty
                     cout << "\nThere is nothing in the Schedule D:\n";
                     cout << "\n----------------------------------------------------\n";
                     break;
                 }
                 else{
-                    Task removed = task.extract();
+                    Task removed = task.extract();  // Call heap extract
                     cout << "\n...Removing Max-Priority Task...\n";
                     cout << "\nTask -> ";
-                    removed.display();
+                    removed.display();  // Show what was removed
                     cout << "Successfully removed :D\n";
                     cout << "\n----------------------------------------------------\n";
                     break;
                 }
                 
             }
-            case 3:{
+            case 3:{        // Display Agenda
                 cout << "\n-----------------------AGENDA-----------------------\n";
                 if (task.isEmpty()){
                     cout << "\nThere is nothing in the Schedule D:\n";
@@ -145,7 +169,7 @@ int main(){
                 cout << "\n############     Please select a number between 0 and 4  >:(     ############\n";
             }
         }
-    }while(selection != 0);
+    }while(selection != 0); // Repeat until user selects 0
 
     return 0;
 }
